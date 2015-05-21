b158ac9827d924d9575377c05568fbc72404a1de
Julien Acroute <julien-github@nura.eu>
2015-05-21 15:47:08 +0200

    New song dialog

---
+++ new_song.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QDialog
+from new_song_ui import Ui_Dialog
+from clip import Song
+
+
+class NewSongDialog(QDialog, Ui_Dialog):
+
+    def __init__(self, parent):
+        super(NewSongDialog, self).__init__(parent)
+        self.gui = parent
+        self.setupUi(self)
+        self.show()
+
+    def accept(self):
+        self.gui.initUI(Song(self.widthSpinBox.value(),
+                             self.heightSpinBox.value()))
+        super(NewSongDialog, self).accept()
